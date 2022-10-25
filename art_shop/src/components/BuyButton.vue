06a7be91b969ff661f40cc6c15bd3d335df3119c
Andrew Smith <91675747+AndrewSmith00@users.noreply.github.com>
2022-10-25 19:40:20 +0300

    Add files via upload

---
+++ art_shop/src/components/BuyButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button 
+    id="buyButton"
+    :class="[
+      card.inBasket ? 'button_in-basket' : '',
+      card.processed ? 'button_processed' : '',
+      'button',
+    ]"
+    :style="[card.sold ? 'display:none' : '']"
+  >
+    {{ card.buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["card"],
+};
+</script>
+
+<style>
+.button {
+  width: 118px;
+  height: 48px;
+  border: none;
+  margin: 0;
+  color: white;
+  font-weight: bold;
+  background-color: #165225;
+  cursor: pointer;
+}
+
+.button_in-basket {
+  background-color: rgb(41, 102, 41);
+}
+
+.button_processed {
+  background-color: #776763;
+}
+
+.button:hover {
+  background-color: #776763;
+}
+</style>
---
+++ art_shop/src/components/header_components/DropDown.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="dropdown" @click="isOpen = !isOpen">
+    <p >Развернуть</p>
+    <transition name="fade" appear>
+      <div class="dropdown__navbar" v-if="isOpen">
+        <div class="navbar__element" v-for="(item, i) in items" v-bind:key="i">
+          <a :href="item.link">{{ item.text }}</a>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+};
+</script>
+
+<style>
+.dropdown {
+  display: none;
+  background-color: fff;
+  height: 44px;
+  min-width: 20%;
+  cursor: pointer;
+  border: 2px solid #9f9f9f;
+  font-family: "Helvetica";
+  font-size: 18px;
+}
+
+.dropdown:hover {
+  background-color: #d8d4d3;
+}
+
+.dropdown__navbar {
+  min-width: 100%;
+  border: 2px solid #9f9f9f;
+  background-color: #fff;
+
+}
+
+.fade-enter-actice, 
+.fade-leave-active {
+  transition: all .5s ease-out;
+}
+
+.fade-enter, 
+.fade-leave-to {
+  opacity: 0;
+}
+
+.navbar__element {
+  text-align: center;
+  padding-top: 7px;
+  padding-bottom: 7px;
+
+}
+
+.navbar__element:hover {
+  background-color: #d8d4d3;
+}
+
+@media screen and (max-width: 940px) {
+  .nav-panel {
+    display: none;
+  }
+
+  .dropdown {
+    display: flex;
+    flex-direction: column;
+    justify-content:flex-start;
+    align-items:center;
+    position: absolute;
+    top: 25px;
+    left: 10%
+  }
+  
+  .dropdown p {
+    margin-top: 13px;
+    padding: 0 10px 0 10px;
+  }
+}
+</style>
